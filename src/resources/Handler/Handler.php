0bb3f4a1ad8ec56d3d98b05b5f3a0515d5e88b4f
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-01 21:44:16 -0300

    Added UnsupportedRequestMethod to GenericResponses

--- src/resources/Handler/Handler.php
+++ src/resources/Handler/Handler.php
@@ -7,6 +7,7 @@
     use Exception;
     use Handler\Abstracts\Module;
     use Handler\GenericResponses\InternalServerError;
+    use Handler\GenericResponses\InvalidUserAgentResponse;
     use Handler\GenericResponses\ModuleListingResponse;
     use Handler\GenericResponses\ResourceNotAvailable;
     use Handler\GenericResponses\ResourceNotFound;
@@ -42,6 +43,7 @@
     require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'GenericResponses' . DIRECTORY_SEPARATOR . 'ResourceNotFound.php');
     require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'GenericResponses' . DIRECTORY_SEPARATOR . 'Root.php');
     require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'GenericResponses' . DIRECTORY_SEPARATOR . 'UnauthorizedResponse.php');
+    require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'GenericResponses' . DIRECTORY_SEPARATOR . 'UnsupportedRequestMethod.php');
     require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'GenericResponses' . DIRECTORY_SEPARATOR . 'UnsupportedVersion.php');
     require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'Objects' . DIRECTORY_SEPARATOR . 'Library.php');
     require_once(HANDLER_DIRECTORY . DIRECTORY_SEPARATOR . 'Objects' . DIRECTORY_SEPARATOR . 'MainConfiguration.php');
@@ -350,17 +352,128 @@
             $RequestRecordEntry->ResponseLength = (int)$module->getContentLength();
             $RequestRecordEntry->ResponseTime = (float)$response_time;
             $RequestRecordEntry->ResponseCode = $module->getResponseCode();
-            $RequestRecordEntry->UserAgent =
+            $RequestRecordEntry->UserAgent = $_SERVER['HTTP_USER_AGENT'];
+            $RequestRecordEntry->IPAddress = self::getClientIP();
+            $RequestRecordEntry->RequestPayload = self::getParameters(true, true);
+            $RequestRecordEntry->RequestMethod = $_SERVER['REQUEST_METHOD'];
 
             //$IntellivoidAPI->getRequestRecordManager()->logRecord(
             //)
         }
 
+        /**
+         * Returns the IP address of the client
+         *
+         * @return string
+         */
+        public static function getClientIP(): string
+        {
+            if(isset($_SERVER['HTTP_CF_CONNECTING_IP']))
+            {
+                return $_SERVER['HTTP_CF_CONNECTING_IP'];
+            }
+
+            if(isset($_SERVER['HTTP_CLIENT_IP']))
+            {
+                return $_SERVER['HTTP_CLIENT_IP'];
+            }
+
+            if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+
+            if(isset($_SERVER['HTTP_X_FORWARDED']))
+            {
+                return $_SERVER['HTTP_X_FORWARDED'];
+            }
+
+            if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            {
+                return $_SERVER['HTTP_FORWARDED_FOR'];
+            }
+
+            if(isset($_SERVER['HTTP_FORWARDED']))
+            {
+                return $_SERVER['HTTP_FORWARDED'];
+            }
+
+            if(isset($_SERVER['REMOTE_ADDR']))
+            {
+                return $_SERVER['REMOTE_ADDR'];
+            }
+
+            if(getenv('HTTP_CLIENT_IP') !== False)
+            {
+                return getenv('HTTP_CLIENT_IP');
+            }
+
+            if(getenv('HTTP_X_FORWARDED_FOR'))
+            {
+                return getenv('HTTP_X_FORWARDED_FOR');
+            }
+
+            if(getenv('HTTP_X_FORWARDED'))
+            {
+                return getenv('HTTP_X_FORWARDED');
+            }
+
+            if(getenv('HTTP_FORWARDED_FOR'))
+            {
+                return getenv('HTTP_FORWARDED_FOR');
+            }
+
+            if(getenv('HTTP_FORWARDED'))
+            {
+                return getenv('HTTP_FORWARDED');
+            }
+
+            if(getenv('REMOTE_ADDR'))
+            {
+                return getenv('REMOTE_ADDR');
+            }
+
+            return '127.0.0.1';
+        }
+
+        /**
+         * Verifies if the request is valid
+         */
         private static function verifyRequest()
         {
             if(isset($_SERVER['HTTP_USER_AGENT']) == false)
             {
+                InvalidUserAgentResponse::executeResponse();
+                exit();
+            }
 
+            if(strlen($_SERVER['HTTP_USER_AGENT']) == 0)
+            {
+                InvalidUserAgentResponse::executeResponse();
+                exit();
+            }
+
+            if(strlen($_SERVER['HTTP_USER_AGENT']) > 624)
+            {
+                InvalidUserAgentResponse::executeResponse();
+                exit();
+            }
+
+            $UnsupportedRequestMethod = true;
+
+            if(strtoupper($_SERVER['REQUEST_METHOD']) == 'GET')
+            {
+                $UnsupportedRequestMethod = false;
+            }
+
+            if(strtoupper($_SERVER['REQUEST_METHOD']) == 'POST')
+            {
+                $UnsupportedRequestMethod = false;
+            }
+
+            if($UnsupportedRequestMethod)
+            {
+                Unsu
             }
         }
 
